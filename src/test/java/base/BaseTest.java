2b0e31b64080526722337f97fc0d6bb85c459256
cristi <c.nacea21@gmail.com>
2026-05-21 19:05:05 +0300

    removed old import

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,7 +1,6 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utilities.ConfigReader;
