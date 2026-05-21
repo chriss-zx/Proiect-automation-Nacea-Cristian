0eb4b5c442629fd779994f6d92cbfb32db9b2535
cristi <c.nacea21@gmail.com>
2026-05-21 20:13:19 +0300

    removed old import; rework tests; added 2 new tests + new comments

--- src/test/java/tests/ContactDetails.java
+++ src/test/java/tests/ContactDetails.java
@@ -6,29 +6,30 @@ import PAGES.LoginPage;
 import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import utilities.WaitUtils;
 
 public class ContactDetails extends BaseTest {
 
-    // mai intai se deschide aplicatia, apoi se face login-ul. daca datele sunt invalide apare mesaj de eroare, iar daca sunt valide se redirectioneaza catre homepage.
-    // se apasa pe index-ul unui contact, iar mai apoi se poate modifica fiecare camp separat. dupa apasarea butonului submit, se face verificare, iar daca datele nu respecta formatul, apare mesaj de eroare si testul pica.
-    // daca datele sunt valide, se apasa butonul de return to homepage, se asteapta ca pagina afisata sa fie homepage-ul, apoi se face click pe index-ul unui contact si se apasa butonul delete.
+    // in acest test se face login-ul, se verifica ca mesajul de eroare sa nu fie afisat, apoi se redirectioneaza catre homepage si se verifica ca pagina sa fie cea corecta, adica homepage.
+    // se apasa pe index-ul unui contact, se verifica ca pagina deschisa sa fie cea corecta, iar mai apoi se poate modifica fiecare camp separat cu date VALIDE. [...]
+    // [...] dupa apasarea butonului submit, se verifica ca mesajul de eroare sa nu fie afisat, apoi se apasa butonul return to homepage si se verifica ca pagina sa fie cea corecta.
 
     @Test
-    public void ContactDetailsTest() {
-
-        HomePage homePage = new HomePage(driver);
+    public void ValidContactDetailsTest() {
 
         LoginPage loginPage = new LoginPage(driver);
         loginPage.loginAs("testc@test.com", "test123");
         loginPage.clickSubmit();
 
         Assert.assertFalse(loginPage.isErrorVisible(), "Credentiale login invalide.");
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactList"), "URL-ul nu contine contactList.");
 
-        homePage.clickOnContact(2);
+        HomePage homePage = new HomePage(driver);
+        homePage.clickOnContact(1);
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactDetails"), "URL-ul nu contine contactDetails.");
 
         ContactPage contactPage = new ContactPage(driver);
         contactPage.clickEditContact();
+        Assert.assertTrue(waitUtils.waitForUrlContains("editContact"), "URL-ul nu contine editContact.");
         contactPage.editFirstName("Maria");
         contactPage.editLastName("ABC");
         contactPage.editBirthdate("1996-06-16");
@@ -40,14 +41,65 @@ public class ContactDetails extends BaseTest {
         contactPage.clickSubmit();
 
         Assert.assertFalse(contactPage.isErrorVisible(), "Datele introduse nu pot fi validate.");
-
-//        contactPage.clickCancel();
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactDetails"), "URL-ul nu contine contactDetails.");
 
         contactPage.returnToHomePage();
 
-        waitUtils.waitForUrlContains("contactList");
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactList"), "URL-ul nu contine contactList.");
+    }
 
-        homePage.clickOnContact(0);
+
+    // in acest test se face login-ul, se verifica ca mesajul de eroare sa nu fie afisat, apoi se redirectioneaza catre homepage si se verifica ca pagina sa fie cea corecta, adica homepage.
+    // se apasa pe index-ul unui contact, se verifica ca pagina deschisa sa fie cea corecta, iar mai apoi se poate modifica fiecare camp separat folosind date INVALIDE. [...]
+    // [...] dupa apasarea butonului submit, se verifica ca mesajul de eroare sa fie afisat
+    @Test
+    public void InvalidContactDetailsTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginAs("testc@test.com", "test123");
+        loginPage.clickSubmit();
+
+        Assert.assertFalse(loginPage.isErrorVisible(), "Credentiale login invalide.");
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactList"), "URL-ul nu contine contactList.");
+
+        HomePage homePage = new HomePage(driver);
+        homePage.clickOnContact(1);
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactDetails"), "URL-ul nu contine contactDetails.");
+
+        ContactPage contactPage = new ContactPage(driver);
+        contactPage.clickEditContact();
+        Assert.assertTrue(waitUtils.waitForUrlContains("editContact"), "URL-ul nu contine editContact.");
+        contactPage.editFirstName("Maria");
+        contactPage.editLastName("ABC");
+        contactPage.editBirthdate("test123");
+        contactPage.editEmail("abc.maria@test.com");
+        contactPage.editPhone("doesn't exist");
+        contactPage.editStreet1("Barcelona");
+        contactPage.editCity("Bucuresti");
+
+        contactPage.clickSubmit();
+
+        Assert.assertTrue(contactPage.isErrorVisible(), "Datele introduse nu pot fi validate.");
+    }
+
+
+    // in acest test se face login-ul, se verifica ca mesajul de eroare sa nu fie afisat, apoi se redirectioneaza catre homepage si se verifica ca pagina sa fie cea corecta, adica homepage.
+    // se apasa pe index-ul unui contact, se verifica ca pagina deschisa sa fie cea corecta si se apasa butonul "Delete Contact".
+    @Test
+    public void DeleteContactTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginAs("testc@test.com", "test123");
+        loginPage.clickSubmit();
+
+        Assert.assertFalse(loginPage.isErrorVisible(), "Credentiale login invalide.");
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactList"), "URL-ul nu contine contactList.");
+
+        HomePage homePage = new HomePage(driver);
+        homePage.clickOnContact(1);
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactDetails"), "URL-ul nu contine contactDetails.");
+
+        ContactPage contactPage = new ContactPage(driver);
         contactPage.deleteContact();
     }
 }
