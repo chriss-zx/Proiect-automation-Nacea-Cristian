6761814bd189732be6a87c4575b740d94de3d022
cristi <c.nacea21@gmail.com>
2026-05-22 01:43:22 +0300

    added base url + endpoints

---
+++ src/test/java/API/config/TestConfig.java
@@ -0,0 +1,12 @@
+package API.config;
+
+public class TestConfig {
+
+    // URL
+    public static final String testerContactList_base_url = "https://thinking-tester-contact-list.herokuapp.com";
+
+    // ENDPOINTS
+    public static final String users_endpoint = "/users";
+    public static final String login_endpoint = users_endpoint + "/login";
+    public static final String contact_endpoint = "/contacts";
+}
