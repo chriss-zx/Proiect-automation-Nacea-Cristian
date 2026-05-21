2f56c31f436f9d54254fae12c1671366fd17797c
cristi <c.nacea21@gmail.com>
2026-05-21 19:19:09 +0300

    tests rework + 2 more tests added

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,6 +4,8 @@ import PAGES.HomePage;
 import PAGES.LoginPage;
 import PAGES.SignupPage;
 import base.BaseTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.WaitUtils;
@@ -11,32 +13,60 @@ import utilities.WaitUtils;
 
 public class LoginTests extends BaseTest {
 
-    // in acest test se deschide site-ul, se apasa butonul de signup, apoi se introduc datele. daca datele sunt invalide, apare mesaj de eroare. daca toate datele sunt valide, contul este creeat si se redirectioneaza catre homepage.
+    private static final Logger log = LoggerFactory.getLogger(LoginTests.class);
+
+    // in acest test se apasa butonul de signup si se introduc datele VALIDE. se verifica daca datele nu sunt folosite, apoi se verifica ca redirect-ul sa fie corect, adica pe homepage.
     @Test
-    public void signupTest() {
+    public void signupTestValid() {
 
         SignupPage signupPage = new SignupPage(driver);
-        signupPage.signupAs("testc", "abcd", "testabc1234@test.com", "test123");
+        signupPage.signupAs("testc", "abcd", "testabc9876@test.com", "test123");
         signupPage.clickSubmit();
 
         Assert.assertFalse(signupPage.isErrorVisible(), "User-ul nu a putut fi creat.");
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactList"), "URL-ul nu contine contactList.");
     }
 
 
-    // in acest test se deschide mai intai site-ul, apoi se introduc datele de login. la final se verifica daca datele sunt valide/invalide. daca sunt valide, se redirectioneaza catre homepage, iar daca sunt invalide apare mesaj de eroare.
+    // in acest test se apasa butonul de signup si se introduc datele INVALIDE, dupa care se verifica ca mesajul de eroare sa fie afisat.
+    @Test
+    public void signupTestInvalid(){
+
+        SignupPage signupPage = new SignupPage(driver);
+        signupPage.signupAs("testc", "abc", "testabc123@test.com", "test123");
+        signupPage.clickSubmit();
+
+        Assert.assertTrue(signupPage.isErrorVisible(), "User-ul nu a putut fi creat.");
+    }
+
+
+
+    // in acest test se introduc datele de login VALIDE, apoi se verifica daca datele sunt valide. daca sunt valide, se redirectioneaza catre homepage si se verifica daca pagina este corecta. la final se apasa butonul logout.
 
     @Test
-    public void loginTest() {
+    public void loginTestValid() {
 
         LoginPage loginPage = new LoginPage(driver);
         loginPage.loginAs("testc@test.com", "test123");
         loginPage.clickSubmit();
 
         Assert.assertFalse(loginPage.isErrorVisible(), "Credentiale login invalide.");
-
-        waitUtils.waitForUrlContains("contactList");
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactList"), "URL-ul nu contine contactList.");
 
         HomePage homePage = new HomePage(driver);
         homePage.clickLogout();
     }
+
+
+    // in acest test se introduc datele de login INVALIDE si se verifica ca mesajul de eroare sa fie afisat.
+    @Test
+    public void loginTestInvalid() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginAs("test", "test123");
+        loginPage.clickSubmit();
+
+        Assert.assertTrue(loginPage.isErrorVisible(), "Credentiale login invalide.");
+    }
+
+
 }
