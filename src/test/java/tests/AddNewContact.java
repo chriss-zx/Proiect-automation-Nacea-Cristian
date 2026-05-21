638784c67334caa93b5ca1e39d7588bf59a9cd0e
cristi <c.nacea21@gmail.com>
2026-05-21 21:44:32 +0300

    rework tests; modified comments; added new asserts & method 'waitForErrorVisible'

--- src/test/java/tests/AddNewContact.java
+++ src/test/java/tests/AddNewContact.java
@@ -9,44 +9,59 @@ import org.testng.annotations.Test;
 
 public class AddNewContact extends BaseTest {
 
-
-    // mai intai se deschide aplicatia, apoi se face login-ul. daca datele sunt invalide apare mesaj de eroare, iar daca sunt valide se redirectioneaza catre homepage.
-    // dupa redirectionarea pe homepage, se apasa butonul de "Add a New Contact" si se introduc datele. campurile firstname si lastname sunt obligatorii, iar anumite campuri (ex. date of birth, email, phone) au un format care trebuie respectat, prin urmare [...]
-    // [...] daca formatul nu este respectat, se face verificare. daca formatul este gresit, apare eroare si testul pica.
-
-    // la primul apel al metodei addNewContact, toate datele sunt valide. dupa completare, se apasa butonul submit, apoi se asteapta ca pagina afisata sa fie homepage-ul, dupa care se apasa din nou "Add a New Contact".
-    // la al 2-lea apel al metodei, anumite date sunt gresite intentionat, ca sa se verifice mesajul de eroare.
-
+    // se face login-ul, se verifica ca mesajul de eroare sa nu fie afisat si se redirectioneaza catre homepage, dupa care se verifica ca pagina sa fie cea corecta.
+    // dupa redirectionarea pe homepage, se apasa butonul de "Add a New Contact", se verifica ca pagina sa fie cea corecta si se introduc datele VALIDE, dupa care se apasa butonul submit, se verifica ca pagina sa fie cea corecta si apoi [...]
+    // [...] se verifica daca contactul a fost adaugat.
 
     @Test
-    public void addNewContactTest() {
-
+    public void addValidContactTest() {
 
         LoginPage loginPage = new LoginPage(driver);
         loginPage.loginAs("testc@test.com", "test123");
         loginPage.clickSubmit();
 
         Assert.assertFalse(loginPage.isErrorVisible(), "Credentiale login invalide.");
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactList"), "URL-ul nu contine contactList.");
 
         HomePage homePage = new HomePage(driver);
         homePage.createNewContact();
 
+        Assert.assertTrue(waitUtils.waitForUrlContains("addContact"), "URL-ul nu contine addContact.");
+
         ContactPage contactPage = new ContactPage(driver);
-        contactPage.addNewContact("Iulia", "Boierescu", "1974/07/27","boierescu.iulia@test.com", "0728468462", "Florilor, 84", "", "Cluj-Napoca","", "283648","Romania");
+        contactPage.addNewContact("Iulia", "Boierescu", "1974/07/27", "boierescu.iulia@test.com", "0728468462", "Florilor, 84", "", "Cluj-Napoca", "", "283648", "Romania");
         contactPage.clickSubmit();
 
-        waitUtils.waitForUrlContains("contactList");
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactList"), "URL-ul nu contine contactList.");
+        Assert.assertTrue(contactPage.isContactPresent("Iulia Boierescu"), "Contactul nu a fost adaugat.");
 
+    }
+
+    // se face login-ul, se verifica ca mesajul de eroare sa nu fie afisat si se redirectioneaza catre homepage, dupa care se verifica ca pagina sa fie cea corecta.
+    // dupa redirectionarea pe homepage, se apasa butonul de "Add a New Contact", se verifica ca pagina sa fie cea corecta si se introduc datele INVALIDE, dupa care se apasa butonul submit, se asteapta ca mesajul de eroare sa apara [...]
+    // [...] si apoi se verifica daca mesajul de eroare este afisat.
+
+    @Test
+    public void addInvalidContactTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginAs("testc@test.com", "test123");
+        loginPage.clickSubmit();
+
+        Assert.assertFalse(loginPage.isErrorVisible(), "Credentiale login invalide.");
+        Assert.assertTrue(waitUtils.waitForUrlContains("contactList"), "URL-ul nu contine contactList.");
+
+        HomePage homePage = new HomePage(driver);
         homePage.createNewContact();
 
-//        addContactPage.clickCancel();
-//
-//        homePage.createNewContact();
+        Assert.assertTrue(waitUtils.waitForUrlContains("addContact"), "URL-ul nu contine addContact.");
 
-        contactPage.addNewContact("Andrei", "Ionescu", "doesn't exist","ionescu.andrei@test", "doesn't exist", "Marului, 726", "Bl. 7, sc. B", "Bucuresti", "Romania", "823664", "Romania");
+        ContactPage contactPage = new ContactPage(driver);
+        contactPage.addNewContact("Andrei", "Ionescu", "doesn't exist", "ionescu.andrei@test", "doesn't exist", "Marului, 726", "Bl. 7, sc. B", "Bucuresti", "Romania", "823664", "Romania");
         contactPage.clickSubmit();
 
-        Assert.assertFalse(homePage.isErrorVisible(), "Datele introduse nu pot fi validate.");
+        contactPage.waitForErrorVisible();
 
+        Assert.assertTrue(homePage.isErrorVisible(), "Datele introduse nu pot fi validate.");
     }
 }
