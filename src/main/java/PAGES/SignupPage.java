91ed9998285c36b03e9d320d66a98f55f9d1e590
cristi <c.nacea21@gmail.com>
2026-05-21 20:11:48 +0300

    removed old imports

--- src/main/java/PAGES/SignupPage.java
+++ src/main/java/PAGES/SignupPage.java
@@ -1,15 +1,9 @@
 package PAGES;
 
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import utilities.WaitUtils;
-
-import java.time.Duration;
 
 public class SignupPage extends BasePage{
 
