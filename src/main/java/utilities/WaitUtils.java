a107add6ad8883f1725c98ec670626c4843ef4e2
cristi <c.nacea21@gmail.com>
2026-05-21 21:31:47 +0300

    added 3 new methods

--- src/main/java/utilities/WaitUtils.java
+++ src/main/java/utilities/WaitUtils.java
@@ -1,5 +1,6 @@
 package utilities;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -23,5 +24,18 @@ public class WaitUtils {
     // metoda asteapta ca un element sa fie vizibil in pagina.
     public WebElement waitForElementVisible(WebElement element) {
         return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void acceptAlert() {
+        wait.until(ExpectedConditions.alertIsPresent()).accept();
+    }
+
+    public void dismissAlert() {
+        wait.until(ExpectedConditions.alertIsPresent()).dismiss();
+    }
+
+    // metoda asteapta ca elementul din pagina sa dispara
+    public boolean waitForContactToDisappear(String name) {
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//td[contains(text(),'" + name + "')]")));
     }
 }
