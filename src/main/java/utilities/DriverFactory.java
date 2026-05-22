3a714e61aff1d03597d4aec6fc05dbc0dd0fd88e
cristi <c.nacea21@gmail.com>
2026-05-22 21:38:56 +0300

    doesn't work, f this

--- src/main/java/utilities/DriverFactory.java
+++ src/main/java/utilities/DriverFactory.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
 
-    public WebDriver driver;
+    public static WebDriver driver;
 
     public static WebDriver getDriver() {
         if(driver == null) {
